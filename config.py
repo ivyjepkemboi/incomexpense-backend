0816a1e575b3aec8614374adf48c974525732685
Ivy <ivyjkemboi@gmail.com>
2025-03-19 20:49:28 +0300

    changed config

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost:5432/expense_tracker"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:admin@localhost:5432/expense_tracker")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your_secret_key"
-
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
