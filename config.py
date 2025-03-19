77d8c1661c4da8ad65be7b307937ee5bc7e16142
Ivy <ivyjkemboi@gmail.com>
2025-03-19 21:44:54 +0300

    Configured PostgreSQL for Render

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:admin@localhost:5432/expense_tracker")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://incomexpense_user:YAOGv0GNJZ3gu2eKByr6kxNHfmPfCB4d@dpg-cvdgu0pc1ekc73e1i1gg-a/incomexpense")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
